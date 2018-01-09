7544c82e42b2004922b13c0b009c92728855a914
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-01-09 12:33:51 -0800

    added animation

---
+++ Assets/AdventurerAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurerAnimation : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	private Rigidbody2D rb;
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Whenever you are using physics, you want to use FixedUpdate, not Update
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+		if (move > 0 &&! facingRight) {
+			Flip();
+		} else if (move < 0 && facingRight) {
+			Flip();
+		}
+	}
+
+	void Flip () {
+		facingRight =! facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
