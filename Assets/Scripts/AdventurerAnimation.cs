6aea7065391eb42b311f739488a2c5b3eee6d289
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-01-10 23:00:08 -0800

    added jump functionality

--- Assets/Scripts/AdventurerAnimation.cs
+++ Assets/Scripts/AdventurerAnimation.cs
@@ -7,8 +7,13 @@ public class AdventurerAnimation : MonoBehaviour {
 	public float maxSpeed = 10f;
 	bool facingRight = true;
 	private Rigidbody2D rb;
+	public float jumpForce = 700f;
 
 	Animator anim;
+	bool grounded = false;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -17,15 +22,31 @@ public class AdventurerAnimation : MonoBehaviour {
 	
 	// Whenever you are using physics, you want to use FixedUpdate, not Update
 	void FixedUpdate () {
+//		lower force and lower gravity can help you adjust gravity and jumps later
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool("Ground", grounded);
+		anim.SetFloat("vSpeed", rb.velocity.y);
+
 		float move = Input.GetAxis("Horizontal");
+//		float jumpY = Input.GetAxis("Vertical");
+
 		anim.SetFloat("Speed", Mathf.Abs(move));
 
 		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+//		HandleJumpAndFall();
 
 		if (move > 0 &&! facingRight) {
 			Flip();
 		} else if (move < 0 && facingRight) {
 			Flip();
+		}
+	}
+
+	void Update() {
+		if (grounded && Input.GetKeyDown(KeyCode.Space)){
+			anim.SetBool("Ground", false);
+			rb.AddForce(new Vector2(0, jumpForce));
 		}
 	}
 
@@ -35,4 +56,15 @@ public class AdventurerAnimation : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+
+	void HandleJumpAndFall ()
+	{
+		if (rb.velocity.y > 0) {
+			anim.SetInteger("State", 3);
+		} else {
+			anim.SetInteger("State", 1);
+		}
+	}
+
+
 }
