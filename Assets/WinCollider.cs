a2b35cdd0a6737123c85e6101ca0dbf4c8d5924d
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-01-12 00:02:18 -0800

    added a win collider to the final tile

---
+++ Assets/WinCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+//	IEnumerator OnTriggerEnter2D(Collider2D trigger) {
+//		yield return new WaitForSeconds(5f);
+//		levelManager = FindObjectOfType<LevelManager>(); 
+//		levelManager.LoadLevel("Win");
+//	}
+
+	IEnumerator OnCollisionEnter2D(Collision2D collision) {
+		print("Collision");		
+		yield return new WaitForSeconds(1f);
+		levelManager = FindObjectOfType<LevelManager>(); 
+		levelManager.LoadLevel("Win");
+	}
+}
