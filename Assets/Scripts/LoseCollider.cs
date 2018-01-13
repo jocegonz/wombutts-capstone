16fe9b8a44151280d9201122e078e1fe1f3f4a26
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-01-13 13:40:31 -0800

    added a lose collider

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	IEnumerator OnTriggerEnter2D(Collider2D trigger) {
+		yield return new WaitForSeconds(.5f);
+		levelManager = FindObjectOfType<LevelManager>(); 
+		levelManager.LoadLevel("Lose");
+	}
+
+	IEnumerator OnCollisionEnter2D(Collision2D collision) {
+		print("Collision");		
+		yield return new WaitForSeconds(.5f);
+		levelManager = FindObjectOfType<LevelManager>(); 
+		levelManager.LoadLevel("Lose");
+	}
+}
