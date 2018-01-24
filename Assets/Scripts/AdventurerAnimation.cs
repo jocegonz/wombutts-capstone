ba69265c5fe1472c0ce58a9347a1dcde0a1ab235
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-01-24 12:38:00 -0800

    working on freezing player when they hit the win collider

--- Assets/Scripts/AdventurerAnimation.cs
+++ Assets/Scripts/AdventurerAnimation.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AdventurerAnimation : MonoBehaviour {
-	private AudioSource audio;
+	public AudioSource audio;
 	private LevelManager levelManager;
 	public bool isInvincible = false;
 
-//	public AudioSource hurt;
+	private bool frozen = false;
 
 	//ANIMATION
 	public float maxSpeed = 10f;
@@ -46,13 +46,19 @@ public class AdventurerAnimation : MonoBehaviour {
 	void FixedUpdate ()
 	{	
 
-//		anim.SetFloat ("vSpeed", rb.velocity.y);
 		if (rb.velocity.y < 0){
 			anim.SetBool("landing", true);
 		}
 
 //		x-axis
 		float move = Input.GetAxis ("Horizontal");
+
+		if (frozen) {
+			move = 0;
+		 	rb.velocity = new Vector2 (0, 0);
+//			anim.SetLayerWeight(1, 0);
+//			isGrounded= false;
+		}
 
 		isGrounded = IsGrounded();
 //		Start running
@@ -61,9 +67,9 @@ public class AdventurerAnimation : MonoBehaviour {
 			rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
 		}
 
-		if (move > 0 &&! facingRight) {
+		if (move > 0 &&! facingRight && !frozen) {
 			Flip();
-		} else if (move < 0 && facingRight) {
+		} else if (move < 0 && facingRight && !frozen) {
 			Flip();
 		}
 
@@ -79,7 +85,7 @@ public class AdventurerAnimation : MonoBehaviour {
 		}
 
 
-		if (isGrounded && jump){
+		if (isGrounded && jump && !false){
 			isGrounded = false;
 			rb.AddForce(new Vector2(0, jumpForce));
 			anim.SetTrigger("jump");
@@ -99,6 +105,9 @@ public class AdventurerAnimation : MonoBehaviour {
 		
 		if (collision.gameObject.tag == "Win") {
 			print ("Collision is happening with win item");	
+			//testing freeze
+			frozen = true;
+
 			anim.SetBool ("won", true);
 		}
 		if (collision.gameObject.tag == "Tiles") {
